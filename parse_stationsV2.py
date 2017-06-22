7a5625a6d725b126b42c4b7bd9e45067f0cc1ba5
ydykid <ydy201@qq.com>
2017-06-22 22:50:58 +0800

    '1.1.1.20170622_Alpha'

---
+++ parse_stationsV2.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+import re
+
+import requests
+
+from pprint import pprint
+
+url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.9015'
+
+response = requests.get(url, verify=False)
+
+# s = response.text.decode("gb2312").encode("utf-8")
+
+# print s
+
+stations = re.findall(u'([\u4e00-\u9fa5]+)\|([A-Z]+)', response.text)
+
+# print("hello world")
+#
+# for s in stations :
+#     print(s[0])
+
+# pprint(stations, indent=4)
+
+# pprint(dict(stations), indent=4)
+
+f = open('stations_all.py','w+',encoding='utf-8')
+f.write('# coding: utf-8\nstations_ch2en = {\n')
+for s in stations :
+    f.write("    '"+s[0]+"':'"+s[1]+"',\n")
+f.write('}')
+f.write('\nstations_en2ch = {\n')
+for s in stations :
+    f.write("    '"+s[1]+"':'"+s[0]+"',\n")
+f.write('}')
+f.close()
--- tickets.py
+++ tickets.py
@@ -28,18 +28,57 @@ import requests
 
 from docopt import docopt
 
-from stations import stations
-# import stations
+from prettytable import PrettyTable
 
+from stations import stations
+from stations_all import stations_en2ch
+
+class TrainsCollection:
+    header = '车次 车次(始终) 路线(始终) 时间 历时 一等 二等 软卧 硬卧 硬座 无座'.split()
+
+    def __init__(self, datas, options, maps):
+        """查询到的火车班次集合
+            :param datas: 数据列表
+            :param options: 查询选项
+            """
+        self.datas = datas
+        self.opts = options
+        self.maps = maps
+
+    @property
+    def trains(self):
+        for data in self.datas :
+            info = data.split('|')
+            opt = info[3][0].lower()
+            if not self.opts or opt in self.opts :
+                train = [
+                    info[3],
+                    '\n'.join([stations_en2ch[info[4]], stations_en2ch[info[5]]]),
+                    '\n'.join([stations_en2ch[info[6]], stations_en2ch[info[7]]]),
+                    '\n'.join([info[8],info[9]]),
+                    info[10],
+                    info[31],
+                    info[30],
+                    info[28],
+                    info[26],
+                    info[25],
+                    info[24],
+                ]
+                yield train
+
+    def pretty_print(self):
+        pt = PrettyTable()
+        pt._set_field_names(self.header)
+        for train in self.trains :
+            pt.add_row(train)
+        print(pt)
 
 def cli():
     """command-line interface"""
     arguments = docopt(__doc__)
     # print(arguments)
     from_station = stations.get(arguments['<from>'])
-
     to_station = stations.get(arguments['<to>'])
-
     date = arguments['<date>']
 
     print('from_station'+from_station)
@@ -53,19 +92,23 @@ def cli():
     url_log = 'https://kyfw.12306.cn/otn/leftTicket/log?purpose_codes=ADULT&leftTicketDTO.train_date={}&leftTicketDTO.from_station={}&leftTicketDTO.to_station={}'.format(
         date, from_station, to_station
     )
+
+    # 获取参数
+    options = ''.join([
+        key for key, value in arguments.items() if value is True
+    ])
+
     # 添加verify=False参数不验证证书
     # r = requests.get(url_log, verify=False)
     # r = requests.get(url_log)
     r = requests.get(url_query, verify=False)
+    r_json = r.json()
+    # print(r)
+    # print(r.url)
+    # print(r.text)
+    # print(r_json['data']['map'])
 
-    print(r)
-
-    print(r.url)
-
-    print(r.text)
-
-    # print(r.json()['data'])
-
+    TrainsCollection(r_json['data']['result'], options, r_json['data']['map']).pretty_print()
 
 if __name__ == '__main__' :
     cli()
